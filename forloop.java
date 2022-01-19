3928275a7ba2f9b346db38a6224fdb4d93b30a51
GaneshRaj06 <97523034+GaneshRaj06@users.noreply.github.com>
2022-01-19 10:43:39 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,11 @@
+public class forloop
+{
+public static void main(String arg[])
+{
+int i;
+for (i=0;i<=10;i++)
+{
+System.out.println(i*2);
+}
+}
+}
---
+++ left shift.java
@@ -0,0 +1,11 @@
+public class left shift
+{
+	public static void main(String args[])
+	 {
+		System.out.println(20>>2);
+		System.out.println(20>>>2);//10*2^2=10*4=40//most significant bit 
+		System.out.println(20>>2);
+		System.out.println(20>>>2);
+	 }	 
+
+}
